ece9cd707c37117ff60c7153c6ace60120407de5
nitish mishra <nitishkumarmishra1@amityonline.com>
2026-06-21 22:14:33 +0530

    console removed

--- server/src/core/db.ts
+++ server/src/core/db.ts
@@ -4,8 +4,6 @@ import { AppSetting } from './setting.js';
 export default async function connectToDatabase() {
     const dbUrl = AppSetting.dbUrl;
 
-    console.log(dbUrl)
-
     if (!dbUrl) {
         throw new Error('MONGO_DB_URL is missing from your environment configuration.');
     }
